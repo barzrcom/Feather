3f51e92490728d43b845b124f50584843fd473ce
Bar Zrihan <bzrihan@drivenets.com>
2019-02-25 23:52:43 +0200

    Init commit

---
+++ Assets/_game/Scripts/LoadSceneOnTrigger.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadSceneOnTrigger : MonoBehaviour {
+
+    public int sceneIndex;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            SceneManager.LoadScene(sceneIndex);
+        }
+    }
+}
---
+++ Assets/_game/UI/MainMenu/Scripts/SelectOnInput.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems; 
+
+public class SelectOnInput : MonoBehaviour {
+
+    public EventSystem eventSystem;
+    public GameObject selectedObject;
+
+    private bool buttonSelected;
+	
+	void Update () {
+        if (Input.GetAxisRaw("Vertical") != 0 && buttonSelected == false) 
+        {
+            eventSystem.SetSelectedGameObject(selectedObject);
+            buttonSelected = true; 
+        }
+	}
+
+    private void OnDisable()
+    {
+        buttonSelected = false; 
+    }
+}
